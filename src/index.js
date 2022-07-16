da15052ac8de9b8dec503bdd4995d9b12cf2f016
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-07-16 18:36:37 +0100

    Copied the comment code to create two more users

--- src/index.js
+++ src/index.js
@@ -19,6 +19,33 @@ const App = () => {
           <div className="text">Nice blog post </div>
         </div>
       </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={faker.image.image()} alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className='author'>
+            Sam
+          </a>
+          <div className="metedata">
+            <span className='date'>Today at 18:00</span>
+          </div>
+          <div className="text">Nice blog post </div>
+        </div>
+      </div>      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={faker.image.image()} alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className='author'>
+            Sam
+          </a>
+          <div className="metedata">
+            <span className='date'>Today at 18:00</span>
+          </div>
+          <div className="text">Nice blog post </div>
+        </div>
+      </div>
     </div>
   );
 };
