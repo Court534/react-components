4ebf40114b9886dabdb837a1906a703c42a814de
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-07-18 18:54:39 +0100

    Playing around with different props in jsx

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import faker from 'faker';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
@@ -5,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail 
+      author="Sam" 
+      timeAgo="Today at 13:00" 
+      comment="Nice blog post"
+      avatar={faker.image.avatar()}
+      />
+      <CommentDetail 
+      author="Alex" 
+      timeAgo="Today at 18:30" 
+      comment="Great post!"
+      avatar={faker.image.avatar()}
+      />
+      <CommentDetail 
+      author="Jane" 
+      timeAgo="Today at 10:00" 
+      comment="Awesome read"
+      avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
