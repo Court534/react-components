b42869d638f3f8840c90836aabb3f144cc850556
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-07-19 18:28:59 +0100

    Gave each user there own seperate box and Approve?/Reject? buttons

--- src/index.js
+++ src/index.js
@@ -7,25 +7,32 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail 
-      author="Sam" 
-      timeAgo="Today at 13:00" 
-      comment="Nice blog post"
-      avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail 
+        author="Sam" 
+        timeAgo="Today at 13:00" 
+        comment="Nice blog post"
+        avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
       <CommentDetail 
       author="Alex" 
       timeAgo="Today at 18:30" 
       comment="Great post!"
       avatar={faker.image.avatar()}
       />
+      </ApprovalCard>
+
+      <ApprovalCard>
       <CommentDetail 
       author="Jane" 
       timeAgo="Today at 10:00" 
       comment="Awesome read"
       avatar={faker.image.avatar()}
       />
+      </ApprovalCard>
     </div>
   );
 };
