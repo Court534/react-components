0fc2915b19e4bd0767e83052e14818ec149da4dc
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-07-19 18:18:11 +0100

    Created a box with with two button 'Approve?' and 'Reject?'

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -7,8 +7,8 @@ const ApprovalCard = () => {
       <div className="content"> Are you Sure? </div>
       <div className="extra content">
         <div className="ui two buttons">
-    <div className="ui basic green button"> Approve? </div>
-    <div className="ui basic green button"> Reject? </div>
+          <div className="ui basic green button"> Approve? </div>
+          <div className="ui basic red button"> Reject? </div>
         </div>
       </div>
     </div>
