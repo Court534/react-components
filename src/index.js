ece0e19e666dc59b93fbc912a5d564cda07aecb9
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-07-20 18:22:25 +0100

    Testing Approvalcard so it can be used in the future for different things

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
+
+    <ApprovalCard>
+      <div>
+        <h4>Warning!</h4>
+        Are you sure you want to do this?
+      </div>
+    </ApprovalCard>
+
       <ApprovalCard>
         <CommentDetail 
         author="Sam" 
@@ -17,21 +25,21 @@ const App = () => {
       </ApprovalCard>
 
       <ApprovalCard>
-      <CommentDetail 
-      author="Alex" 
-      timeAgo="Today at 18:30" 
-      comment="Great post!"
-      avatar={faker.image.avatar()}
-      />
+        <CommentDetail 
+        author="Alex" 
+        timeAgo="Today at 18:30" 
+        comment="Great post!"
+        avatar={faker.image.avatar()}
+        />
       </ApprovalCard>
 
       <ApprovalCard>
-      <CommentDetail 
-      author="Jane" 
-      timeAgo="Today at 10:00" 
-      comment="Awesome read"
-      avatar={faker.image.avatar()}
-      />
+        <CommentDetail 
+        author="Jane" 
+        timeAgo="Today at 10:00" 
+        comment="Awesome read"
+        avatar={faker.image.avatar()}
+        />
       </ApprovalCard>
     </div>
   );
